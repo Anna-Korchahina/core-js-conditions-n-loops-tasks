844db3c36ebffc26a12367214716a8ad578e71d7
Anna-Korchahina <korchagina.anna.v@gmail.com>
2024-01-07 17:18:03 +0200

    feat: do task getIndexOf

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -213,8 +213,20 @@ function isPalindrome(/* str */) {
  *  'qwerty', 'Q'     => -1
  *  'qwerty', 'p'     => -1
  */
-function getIndexOf(/* str, letter */) {
-  throw new Error('Not implemented');
+function getIndexOf(str, letter) {
+  let res = null;
+  for (let i = 0; i < str.length; i += 1) {
+    if (str[i] === letter) {
+      res = i;
+      break;
+    }
+  }
+
+  if (res === null) {
+    return -1;
+  }
+
+  return res;
 }
 
 /**
