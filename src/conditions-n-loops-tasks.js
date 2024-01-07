0bb6fbfce7c1535f328ad77a044d7909d206a02d
Anna-Korchahina <korchagina.anna.v@gmail.com>
2024-01-07 16:43:49 +0200

    feat: do task getMaxNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -41,8 +41,16 @@ function isPositive(number) {
  *  -5, 0, 5      => 5
  *  -0.1, 0, 0.2  => 0.2
  */
-function getMaxNumber(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getMaxNumber(a, b, c) {
+  let maxValue = a;
+  if (maxValue < b) {
+    maxValue = b;
+  }
+  if (maxValue < c) {
+    maxValue = c;
+  }
+
+  return maxValue;
 }
 
 /**
